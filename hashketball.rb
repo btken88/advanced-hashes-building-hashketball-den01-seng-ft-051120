8713ceddf5dcf139f878a253559f00d2c0978378
Bryce Kennedy <btken88@gmail.com>
2020-04-23 19:32:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -87,6 +87,28 @@ def game_hash
         :steals => 22,
         :blocks => 15,
         :slam_dunks => 10
+      },
+      {
+        :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
+      },
+      {
+        :player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => ,
+        :steals => ,
+        :blocks => ,
+        :slam_dunks =>
       }
       ]
   }
